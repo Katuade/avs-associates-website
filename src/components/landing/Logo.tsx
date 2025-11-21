db5913c218631f58fd68c20c79cb4ad548906b34
Nirmal Singh Katuade <Nirmalsinghkatuade@gmail.com>
2025-11-21 17:56:03 +0000

    use the avs_logo.png in the static folder

--- src/components/landing/Logo.tsx
+++ src/components/landing/Logo.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative h-10 w-48", className)}>
+    <div className={cn("relative h-12 w-48", className)}>
       <Image
-        src="https://storage.googleapis.com/aiv-studio-assets/avs-logo.png"
+        src="/avs_logo.png"
         alt="AVS Associates Logo"
         fill
         className="object-contain"
